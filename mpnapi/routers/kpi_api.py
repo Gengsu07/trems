1b6536cdaac84d8be57148485030b63b18e04941
Sugeng Wahyudi <sugengdatascience@gmail.com>
2023-12-03 15:05:31 +0700

    add multi filter for bruto

--- mpnapi/routers/kpi_api.py
+++ mpnapi/routers/kpi_api.py
@@ -1,10 +1,10 @@
 from datetime import date
-from typing import List
+from typing import List, Optional
 
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 from fastapi import APIRouter, Depends, Query
-from sqlmodel import Session, case, func, or_, select
+from sqlmodel import Session, and_, case, func, or_, select
 
 from ..database.db import get_db
 from ..models.ppmpkm import ppmpkm, ppmpkm_base
@@ -35,26 +35,47 @@ def get_bruto(
     tgl_akhir: date = Query(
         date.today(), description="Tanggal akhir, default hari ini"
     ),
+    sektor: Optional[str] = None,
+    kdmap: Optional[str] = None,
+    npwp15: Optional[str] = None,
+    nama_wp: Optional[str] = None,
     session: Session = Depends(get_db),
 ):
+    # kita bikin base condition dlu, yaitu filter by date awal tahun sampai sekarang dan tahun yg lalu waktu yg sama
+    base_stmt = or_(
+        ppmpkm.datebayar.between(tgl_awal, tgl_akhir),
+        ppmpkm.datebayar.between(
+            tgl_awal - relativedelta(years=1), tgl_akhir - relativedelta(years=1)
+        ),
+    )
+
+    # kalau ada kdmap atau sektor atau filter yg lain maka kita tambahkan kondisinya pakai and_
+    if kdmap:
+        base_stmt = and_(base_stmt, ppmpkm.kdmap == kdmap)
+    if sektor:
+        base_stmt = and_(base_stmt, ppmpkm.kd_kategori == sektor)
+    if npwp15:
+        base_stmt = and_(base_stmt, ppmpkm.npwp15 == npwp15)
+    if nama_wp:
+        base_stmt = and_(base_stmt, ppmpkm.nama_wp == nama_wp)
+
     stmt = select(
         func.sum(case((ppmpkm.tahunbayar == tgl_awal.year, ppmpkm.nominal))).label(
-            "Bruto_CY"
+            "bruto_cy"
         ),
         func.sum(case((ppmpkm.tahunbayar == tgl_awal.year - 1, ppmpkm.nominal))).label(
-            "Bruto_PY"
+            "bruto_py"
         ),
-    ).where(
-        or_(
-            ppmpkm.datebayar.between(tgl_awal, tgl_akhir),
-            ppmpkm.datebayar.between(
-                tgl_awal - relativedelta(years=1), tgl_akhir - relativedelta(years=1)
-            ),
-        )
-    )
+    ).where(base_stmt)
 
     data = session.exec(stmt).all()
     results = [
-        {"Bruto_CY": Bruto_CY, "Bruto_PY": Bruto_PY} for Bruto_CY, Bruto_PY in data
+        {
+            "bruto_cy": int(bruto_cy),
+            "bruto_py": int(bruto_py),
+            "selisih_bruto": int(bruto_cy - bruto_py),
+            "%naik": round(((bruto_cy - bruto_py) / bruto_py) * 100, 2),
+        }
+        for bruto_cy, bruto_py in data
     ]
     return results
